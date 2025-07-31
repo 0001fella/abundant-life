b6fc29ab21817be8dd8916add6c7bd53b288d257
0001fella <fanteskorri36@gmail.com>
2025-08-01 00:21:31 +0300

    modified:home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
-import About from "./About";
-import Testimonials from "./Testimonials";
+
 
 
 const Home = () => {
   return (
     <div className="pt-24 space-y-10 bg-theme text-theme">
       <Hero />
-      <About />
-      <Testimonials />
+      
     
     </div>
   );
